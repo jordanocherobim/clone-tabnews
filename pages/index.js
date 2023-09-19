1f378db7a7bac7dfb80bb6c09a7df5b644d09e78
jordanocherobim <32414319+jordanocherobim@users.noreply.github.com>
2023-09-19 00:11:28 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Esta é uma frase qualquer, apenas para teste. É um outro teste qualquer. Também, eu acho.</h1>
+  return (
+    <h1>
+      Esta é uma frase qualquer, apenas para teste. É um outro teste qualquer.
+      Também, eu acho.
+    </h1>
+  );
 }
 
 export default Home;
